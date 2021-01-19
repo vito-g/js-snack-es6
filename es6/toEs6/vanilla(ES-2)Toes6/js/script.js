a1d978d610ef0d6982c2b9d9f8fd7216e8910be0
Vito Gentile <gentilevito1076@gmail.com>
2021-01-19 17:24:01 +0100

    ESERCIZIO 2 (Es6) - Migliorato Console log

---
+++ es6/toEs6/vanilla(ES-2)Toes6/js/script.js
@@ -0,0 +1,36 @@
+// jsnack 2
+// Creare un array di oggetti di squadre di calcio.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti
+// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const squadre = [
+  {'nome' : 'Juve' , 'score' : 0, 'falliSubiti' : 0 },
+  {'nome' : 'Napoli' , 'score' : 0, 'falliSubiti' : 0 },
+  {'nome' : 'Inter' , 'score' : 0, 'falliSubiti' : 0 }
+];
+
+function random(a, b) {
+  var randomNumber = Math.floor((Math.random() * b) + a);
+  return randomNumber;
+}
+
+for (let i = 0; i < squadre.length; i++) {
+  squadre[i].score = random(0,100);
+  squadre[i].falliSubiti = random(0,20);
+
+}
+
+console.log('Array 1 ', squadre);
+
+// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const squadreReview = [];
+
+for (let i = 0; i < squadre.length; i++) {
+  squadreReview.push({nome : squadre[i].nome, falliSubiti : squadre[i].falliSubiti });
+}
+
+
+console.log('Array 2 ', squadreReview);
