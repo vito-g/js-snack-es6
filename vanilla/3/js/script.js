b5a319231b3e76cb932d33d0cf43470239ba7aa5
Vito Gentile <gentilevito1076@gmail.com>
2021-01-20 15:47:24 +0100

    jsnacks 3 -Es5 - OK - Stavolta con CICLO FOR - COMPLETATO

--- vanilla/3/js/script.js
+++ vanilla/3/js/script.js
@@ -2,11 +2,31 @@
 // Si scriva una funzione che accetti tre argomenti, un array e due numeri (A più piccolo di B).
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri
 // L'esercizio sarà da svolgere prima in ES5 poi in ES6.
+// --------------------------------------------------
+//SEnza CICLO FOR:
+
+// var list = [23,54,89,4,2,90];
+// function fx(array,numMin,numMax) {
+//   return console.log(array.slice(numMin,numMax));//crea un nuovo array a partire da quello selezionato e costituito dagli elementi dell'array selezionato con indici a partire da numMin sino a numMax(non incluso)
+// }
+//
+// fx(list, 1, 4);
+
+// ---------------------------------------------------
+// COL CICLO for
+var min = 1;
+var max = 3;
 
 
 var list = [23,54,89,4,2,90];
 function fx(array,numMin,numMax) {
-  return console.log(array.slice(numMin,numMax));//crea un nuovo array a partire da quello selezionato e costituito dagli elementi dell'array selezionato con indici a partire da numMin sino a numMax(non incluso)
+  var arrayNew = [];
+  for ( var i = 0; i < list.length; i++) {
+    if (( i >= min) && (i <= max)) {
+    arrayNew.push(list[i]);
+  }
+}
+return arrayNew;
 }
 
-fx(list, 1, 4);
+console.log(fx(list, min, max));
