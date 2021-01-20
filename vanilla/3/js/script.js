2e9f05aca0b0ac052142ccb4bc03d690a80c00d1
Vito Gentile <gentilevito1076@gmail.com>
2021-01-20 13:56:52 +0100

    Commento aggiunto - Jsnack3 - Es5 - COMPLETATO

--- vanilla/3/js/script.js
+++ vanilla/3/js/script.js
@@ -6,7 +6,7 @@
 
 var list = [23,54,89,4,2,90];
 function fx(array,numMin,numMax) {
-  return console.log(array.slice(numMin,numMax));
+  return console.log(array.slice(numMin,numMax));//crea un nuovo array a partire da quello selezionato e costituito dagli elementi dell'array selezionato con indici a partire da numMin sino a numMax(non incluso)
 }
 
 fx(list, 1, 4);
