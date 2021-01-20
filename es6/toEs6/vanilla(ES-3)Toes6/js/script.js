78e34d3bb24301d83d07a4789856a36db9861e15
Vito Gentile <gentilevito1076@gmail.com>
2021-01-20 13:59:20 +0100

    Jsnack3 - Es6- START Files

---
+++ es6/toEs6/vanilla(ES-3)Toes6/js/script.js
@@ -0,0 +1,4 @@
+// jsnack 3
+// Si scriva una funzione che accetti tre argomenti, un array e due numeri (A più piccolo di B).
+// La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri
+// L'esercizio in ES6.
